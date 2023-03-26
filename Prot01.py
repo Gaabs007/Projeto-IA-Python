3f7dd687767b87d1a9a6e156272b3233c43f330f
gaabs007 <acesso.gap@gmail.com>
2023-03-26 17:59:28 -0400

    Projeto de estudo IA (Python)

---
+++ Prot01.py
@@ -0,0 +1,8 @@
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+# Calcula a média dos dois valores
+media = (valor1 + valor2) / 2
+
+# Imprime o resultado
+print("A média dos valores", valor1, "e", valor2, "é:", media)
